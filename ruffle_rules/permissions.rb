a9f76ffa0248a7a629f5a0c95a13b6e9143f10d1
Colin Gan <beshimi@hotmail.com>
2008-12-15 02:05:06 +0900

    My first attempt at breaking up an app into pieces

---
+++ ruffle_rules/permissions.rb
@@ -0,0 +1,24 @@
+module Permissions
+  def Permissions.check(files)
+    files.each do |file| 
+      #puts file[5] if file[0].start_with?('dr') #view directories
+      #puts file[5] #view directories
+      #puts file[0]
+      #puts file[0].scan(/.?/).each {|x| puts x}
+      case 
+      when file[0].start_with?("d")
+        puts "Is a directory"
+        puts "Is executable" if file[0].end_with?("x")
+      when file[0].start_with?("-")
+        puts "Is a file"
+        puts file[0]
+        #the following are inverted to aid testing
+        puts "Is not world writable" if not file[0][-2..-2] == "w"
+        puts "Is world readable" if file[0][-3..-3] == "r"
+      when file[0].start_with?("l")
+        puts "Is a link"
+      end
+    end
+  end
+
+end
